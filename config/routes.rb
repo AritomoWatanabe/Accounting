8eb06923badebfc7bc51e131a1e3f76a5c6f4a22
渡辺有智 <aritomo@aritomo.local>
2020-09-20 23:55:06 +0900

    現場モデルなどの追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   get 'search' => 'accounts#search'
   resources :accounts, only:[:index, :create, :destroy]
 
+  resources :construction_sites, only:[:new, :create]
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_23_105119) do
+ActiveRecord::Schema.define(version: 2020_09_20_141906) do
 
   create_table "accounts", force: :cascade do |t|
     t.integer "year", null: false
@@ -18,6 +18,12 @@ ActiveRecord::Schema.define(version: 2020_08_23_105119) do
     t.integer "day", null: false
     t.string "item", null: false
     t.integer "price", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "construction_sites", force: :cascade do |t|
+    t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
